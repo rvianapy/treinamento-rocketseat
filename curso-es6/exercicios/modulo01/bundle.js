b678301f0469a71dbf15903da41081a795c51483
Rodrigo Viana de Oliveira <roviol@gmail.com>
2020-07-10 00:35:16 -0300

    Exercícios do módulo 1

--- curso-es6/exercicios/modulo01/bundle.js
+++ curso-es6/exercicios/modulo01/bundle.js
@@ -1,19 +1,53 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var usuarios = [{
   nome: 'Diego',
   idade: 23,
   empresa: 'Rocketseat'
 }, {
   nome: 'Gabriel',
-  idade: 18,
+  idade: 17,
   empresa: 'Rocketseat'
 }, {
   nome: 'Lucas',
   idade: 30,
   empresa: 'Facebook'
-}];
+}, {
+  nome: 'Rodrigo',
+  idade: 40,
+  empresa: 'Google'
+}]; // Retorna as idades dos usuários
+
 var arrayIdades = usuarios.map(function (usuario) {
   return usuario.idade;
 });
-console.log(arrayIdades);
+console.log(arrayIdades); // Retorna usuários com mais de 18 anos e que trabalham na Rocketseat
+
+var newArr = usuarios.filter(function (usuario) {
+  return usuario.idade >= 18 && usuario.empresa === 'Rocketseat';
+});
+console.log(newArr); // Retorna usuários que trabalham no Google
+
+var _usuarios$find = usuarios.find(function (usuario) {
+  return usuario.empresa === 'Google';
+}),
+    nome = _usuarios$find.nome;
+
+console.log(nome); // Dobra as idades dos usuários e retorna aqueles com no máximo 50 anos
+
+var newAges = usuarios.map(function (usuario) {
+  var newUser = _objectSpread({}, usuario);
+
+  newUser.idade = newUser.idade * 2;
+  return newUser;
+});
+var underFifty = newAges.filter(function (usuario) {
+  return usuario.idade <= 50;
+});
+console.log(underFifty);
--- curso-es6/exercicios/modulo01/exercicio02.js
+++ curso-es6/exercicios/modulo01/exercicio02.js
@@ -1,9 +1,36 @@
 const usuarios = [
   {nome: 'Diego', idade: 23, empresa: 'Rocketseat'},
-  {nome: 'Gabriel', idade: 18, empresa: 'Rocketseat'},
-  {nome: 'Lucas', idade: 30, empresa: 'Facebook'}
+  {nome: 'Gabriel', idade: 17, empresa: 'Rocketseat'},
+  {nome: 'Lucas', idade: 30, empresa: 'Facebook'},
+  {nome: 'Rodrigo', idade: 40, empresa: 'Google'},
 ];
 
+
+// Retorna as idades dos usuários
 const arrayIdades = usuarios.map(usuario => usuario.idade);
 
 console.log(arrayIdades);
+
+
+// Retorna usuários com mais de 18 anos e que trabalham na Rocketseat
+const newArr = usuarios.filter(usuario => usuario.idade >= 18 && usuario.empresa === 'Rocketseat');
+
+console.log(newArr);
+
+
+// Retorna usuários que trabalham no Google
+const {nome} = usuarios.find(usuario => usuario.empresa === 'Google');
+
+console.log(nome);
+
+
+// Dobra as idades dos usuários e retorna aqueles com no máximo 50 anos
+const newAges = usuarios.map(usuario => {
+  const newUser = {...usuario};
+  newUser.idade = newUser.idade * 2;
+  return newUser;
+});
+
+const underFifty = newAges.filter(usuario => usuario.idade <= 50);
+
+console.log(underFifty);
