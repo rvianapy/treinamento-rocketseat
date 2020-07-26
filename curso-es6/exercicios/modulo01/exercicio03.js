2c746a452e8c6df557fcc64cbeede62ed73bb870
Rodrigo Viana de Oliveira <roviol@gmail.com>
2020-07-26 20:53:00 -0300

    Mais exercícios do módulo 1

--- curso-es6/exercicios/modulo01/exercicio03.js
+++ curso-es6/exercicios/modulo01/exercicio03.js
@@ -1,28 +1,25 @@
-"use strict";
-
 // Converter as funções nos seguintes trechos de código em Arrow Functions
+
 // 3.1
+
 // arr.map(function(item) {
 //   return item + 10;
 // });
-var arr = [1, 2, 3, 4, 5];
-var newArr = arr.map(function (item) {
-  return item + 10;
-});
-console.log(newArr); // 3.2
+
+const arr = [1, 2, 3, 4, 5];
+const newArr = arr.map(item => item + 10);
+console.log(newArr);
+
+
+// 3.2
+
 // const usuario = {nome: 'Diego', idade: 23};
 // function mostraIdade(usuario) {
 //   return usuario.idade;
 // }
 // mostraIdade(usuario);
 
-var usuario = {
-  nome: 'Diego',
-  idade: 23
-};
 
-var mostraIdade = function mostraIdade() {
-  return usuario.idade;
-};
-
+const usuario = {nome: 'Diego', idade: 23};
+const mostraIdade = () => usuario.idade;
 console.log(mostraIdade);
