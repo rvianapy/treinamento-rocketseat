7db27d0c1491bc36616f8b32d6a72f8251a1eeb0
Rodrigo Viana de Oliveira <roviol@gmail.com>
2020-07-27 23:09:11 -0300

    Exercício 4.2 do módulo 1

--- curso-es6/exercicios/modulo01/bundle.js
+++ curso-es6/exercicios/modulo01/bundle.js
@@ -25,4 +25,81 @@ var mostraIdade = function mostraIdade() {
   return usuario.idade;
 };
 
-console.log(mostraIdade());
+console.log(mostraIdade()); // 3.3
+// const nome = "Diego";
+// const idade = 23;
+// function mostraUsuario(nome = 'Diego', idade = 18) {
+//   return {nome, idade};
+// }
+// console.log(mostraUsuario(nome, idade));
+// console.log(mostraUsuario(nome));
+
+var nome = "Diego";
+var idade = 23;
+
+var mostraUsuario = function mostraUsuario() {
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario());
+
+mostraUsuario = function mostraUsuario() {
+  return nome;
+};
+
+console.log(mostraUsuario()); // 3.4
+// const promise = function() {
+//   return new Promise(function(resolve, reject) {
+//     return resolve();
+//   })
+// }
+// DESESTRUTURAÇÃO
+// A partir do seguinte objeto:
+//
+// const empresa = {
+//   nome: 'Rocketseat',
+//   endereco: {
+//     cidade: 'Rio do Sul',
+//     estado: 'SC',
+//   }
+// };
+// 4.1
+// Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis. No fim, deve ser possível fazer o seguinte:
+// console.log(nome); // Rocketseat
+// console.log(cidade); // Rio do Sul
+//console.log(estado); // SC
+
+var empresa = {
+  nomeEmpresa: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nomeEmpresa = empresa.nomeEmpresa,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nomeEmpresa);
+console.log(cidade);
+console.log(estado); // 4.2
+// Na seguinte função:
+// function mostraInfo(usuario) {
+//   return `${usuario.nome} tem ${usuario.idade} anos.`;
+// }
+// mostraInfo({nome: 'Diego', idade: 23})
+// Utilize a desestruturação nos parâmetros 
+// da função para buscar o nome e idade do usuário
+// separadamente e a função poder retornar apenas:
+// return `${nome} tem ${idade} anos.`;
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo(usuario));
--- curso-es6/exercicios/modulo01/exercicio03.js
+++ curso-es6/exercicios/modulo01/exercicio03.js
@@ -23,3 +23,93 @@ console.log(newArr);
 const usuario = {nome: 'Diego', idade: 23};
 const mostraIdade = () => usuario.idade;
 console.log(mostraIdade());
+
+
+// 3.3
+
+// const nome = "Diego";
+// const idade = 23;
+
+// function mostraUsuario(nome = 'Diego', idade = 18) {
+//   return {nome, idade};
+// }
+
+// console.log(mostraUsuario(nome, idade));
+// console.log(mostraUsuario(nome));
+
+const nome = "Diego";
+const idade = 23;
+
+var mostraUsuario = () => ({nome, idade});
+console.log(mostraUsuario());
+mostraUsuario = () => nome;
+console.log(mostraUsuario());
+
+
+// 3.4
+
+// const promise = function() {
+//   return new Promise(function(resolve, reject) {
+//     return resolve();
+//   })
+// }
+
+
+
+// DESESTRUTURAÇÃO
+
+// A partir do seguinte objeto:
+//
+// const empresa = {
+//   nome: 'Rocketseat',
+//   endereco: {
+//     cidade: 'Rio do Sul',
+//     estado: 'SC',
+//   }
+// };
+
+// 4.1
+
+// Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis. No fim, deve ser possível fazer o seguinte:
+
+// console.log(nome); // Rocketseat
+// console.log(cidade); // Rio do Sul
+//console.log(estado); // SC
+
+const empresa = {
+  nomeEmpresa: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
+  },
+};
+
+const {nomeEmpresa, endereco: {cidade, estado}} = empresa;
+
+console.log(nomeEmpresa);
+console.log(cidade);
+console.log(estado);
+
+
+// 4.2
+
+// Na seguinte função:
+
+// function mostraInfo(usuario) {
+//   return `${usuario.nome} tem ${usuario.idade} anos.`;
+// }
+
+// mostraInfo({nome: 'Diego', idade: 23})
+
+// Utilize a desestruturação nos parâmetros 
+// da função para buscar o nome e idade do usuário
+// separadamente e a função poder retornar apenas:
+
+// return `${nome} tem ${idade} anos.`;
+
+
+function mostraInfo({nome, idade}) {
+  return `${nome} tem ${idade} anos.`;
+}
+
+console.log(mostraInfo(usuario));
