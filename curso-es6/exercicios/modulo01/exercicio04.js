ddfc9cc5bb9890e44c8cbbefcf081ccdda3ae99d
Rodrigo Viana de Oliveira <roviol@gmail.com>
2020-07-29 00:13:22 -0300

    Exercícios de desestruturação no arquivo exercicio04.js

--- curso-es6/exercicios/modulo01/exercicio04.js
+++ curso-es6/exercicios/modulo01/exercicio04.js
@@ -1,6 +1,5 @@
-"use strict";
-
 // DESESTRUTURAÇÃO
+
 // A partir do seguinte objeto:
 //
 // const empresa = {
@@ -10,44 +9,50 @@
 //     estado: 'SC',
 //   }
 // };
+
 // 4.1
+
 // Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis. No fim, deve ser possível fazer o seguinte:
+
 // console.log(nome); // Rocketseat
 // console.log(cidade); // Rio do Sul
 //console.log(estado); // SC
-var empresa = {
+
+const empresa = {
   nome: 'Rocketseat',
   endereco: {
     cidade: 'Rio do Sul',
-    estado: 'SC'
-  }
+    estado: 'SC',
+  },
 };
-var nome = empresa.nome,
-    _empresa$endereco = empresa.endereco,
-    cidade = _empresa$endereco.cidade,
-    estado = _empresa$endereco.estado;
+
+const {nome, endereco: {cidade, estado}} = empresa;
+
 console.log(nome);
 console.log(cidade);
-console.log(estado); // 4.2
+console.log(estado);
+
+
+// 4.2
+
 // Na seguinte função:
+
 // function mostraInfo(usuario) {
 //   return `${usuario.nome} tem ${usuario.idade} anos.`;
 // }
+
 // mostraInfo({nome: 'Diego', idade: 23})
+
 // Utilize a desestruturação nos parâmetros 
 // da função para buscar o nome e idade do usuário
 // separadamente e a função poder retornar apenas:
+
 // return `${nome} tem ${idade} anos.`;
 
-var usuario = {
-  nome: 'Diego',
-  idade: 23
-};
+const usuario = {nome: 'Diego', idade: 23};
 
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
+function mostraInfo({nome, idade}) {
+  return `${nome} tem ${idade} anos.`;
 }
 
 console.log(mostraInfo(usuario));
