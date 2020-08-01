ef568ad264babb3ef5002abdbd2e4c8ad49d5d01
Rodrigo Viana de Oliveira <roviol@gmail.com>
2020-07-31 22:56:03 -0300

    Finalização dos exercícios do módulo 1

--- curso-es6/exercicios/modulo01/exercicio07.js
+++ curso-es6/exercicios/modulo01/exercicio07.js
@@ -1,20 +1,26 @@
-"use strict";
-
 // OBJECT SHORT SYNTAX (sintaxe curta de objeto)
+
+
 // 7
+
 // Utilize a sintaxe curta de objeto no seguinte objeto:
+
 // const nome = 'Diego';
 // const idade = 23;
+
 // const usuario = {
 //   nome: nome,
 //   idade: idade,
 //   cidade: 'Rio do Sul',
 // };
-var nome = 'Diego';
-var idade = 23;
-var usuario = {
-  nome: nome,
-  idade: idade,
-  cidade: 'Rio do Sul'
+
+const nome = 'Diego';
+const idade = 23;
+
+const usuario = {
+  nome,
+  idade,
+  cidade: 'Rio do Sul',
 };
+
 console.log(usuario);
